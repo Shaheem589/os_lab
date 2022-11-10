eccda59d8fa559e5b1bba1e0d6d42920a171ff96
Mohammed Shaheem C <77876823+Shaheem589@users.noreply.github.com>
2022-11-10 11:58:15 +0530

    Add files via upload

---
+++ stage_12/stage_12.sh
@@ -0,0 +1,8 @@
+load --init $HOME/os_lab_stages/stage_12/numbers1-100.xsm
+load --idle $HOME/os_lab_stages/stage_12/idle_process.xsm
+load --os $HOME/os_lab_stages/stage_12/os_startup.xsm
+load --int=timer $HOME/os_lab_stages/stage_12/timer.xsm
+load --int=10 $HOME/os_lab_stages/stage_12/haltprog.xsm
+load --int=7 $HOME/os_lab_stages/stage_12/int_7.xsm
+load --exhandler $HOME/os_lab_stages/stage_12/haltprog.xsm
+load --library $HOME/myexpos/expl/library.lib
