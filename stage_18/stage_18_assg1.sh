a58cef91b84a9c2be4ff9ea88f58f76add3f6832
Mohammed Shaheem C <77876823+Shaheem589@users.noreply.github.com>
2022-11-10 12:05:22 +0530

    Add files via upload

---
+++ stage_18/stage_18_assg1.sh
@@ -0,0 +1,20 @@
+load --idle $HOME/os_lab_stages/stage_18/idle_process.xsm
+load --init $HOME/os_lab_stages/stage_18/input.xsm
+load --exec $HOME/os_lab_stages/stage_18/odd.xsm
+load --exec $HOME/os_lab_stages/stage_18/even.xsm
+load --exec $HOME/os_lab_stages/stage_18/prime.xsm
+load --exhandler $HOME/os_lab_stages/stage_18/exhandler.xsm
+load --os $HOME/os_lab_stages/stage_18/os_startup.xsm
+load --int=timer $HOME/os_lab_stages/stage_18/timer.xsm
+load --int=console $HOME/os_lab_stages/stage_18/console.xsm
+load --int=disk $HOME/os_lab_stages/stage_18/disk.xsm
+load --int=6 $HOME/os_lab_stages/stage_18/int_6.xsm
+load --int=7 $HOME/os_lab_stages/stage_18/int_7.xsm
+load --int=9 $HOME/os_lab_stages/stage_18/int_9.xsm
+load --int=10 $HOME/os_lab_stages/stage_18/int_10.xsm
+load --module 0 $HOME/os_lab_stages/stage_18/module_0.xsm
+load --module 1 $HOME/os_lab_stages/stage_18/module_1.xsm
+load --module 2 $HOME/os_lab_stages/stage_18/module_2.xsm
+load --module 4 $HOME/os_lab_stages/stage_18/module_4.xsm
+load --module 5 $HOME/os_lab_stages/stage_18/module_5.xsm
+load --module 7 $HOME/os_lab_stages/stage_18/module_7.xsm
---
+++ stage_21/stage_21_assg1.sh
@@ -0,0 +1,25 @@
+load --idle $HOME/os_lab_stages/stage_21/idle_process.xsm
+load --init $HOME/os_lab_stages/stage_21/shell.xsm
+load --exec $HOME/os_lab_stages/stage_21/odd.xsm
+load --exec $HOME/os_lab_stages/stage_21/prime.xsm
+load --exec $HOME/os_lab_stages/stage_21/even.xsm
+load --exec $HOME/os_lab_stages/stage_21/pid.xsm
+load --os $HOME/os_lab_stages/stage_21/os_startup.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/os_lab_stages/stage_21/exhandler.xsm
+load --int=timer $HOME/os_lab_stages/stage_21/timer.xsm
+load --int=console $HOME/os_lab_stages/stage_21/console.xsm
+load --int=disk $HOME/os_lab_stages/stage_21/disk.xsm
+load --int=6 $HOME/os_lab_stages/stage_21/int_6.xsm
+load --int=7 $HOME/os_lab_stages/stage_21/int_7.xsm
+load --int=8 $HOME/os_lab_stages/stage_21/int_8.xsm
+load --int=9 $HOME/os_lab_stages/stage_21/int_9.xsm
+load --int=10 $HOME/os_lab_stages/stage_21/int_10.xsm
+load --int=11 $HOME/os_lab_stages/stage_21/int_11.xsm
+load --int=15 $HOME/os_lab_stages/stage_21/int_15.xsm
+load --module 0 $HOME/os_lab_stages/stage_21/module_0.xsm
+load --module 1 $HOME/os_lab_stages/stage_21/module_1.xsm
+load --module 2 $HOME/os_lab_stages/stage_21/module_2.xsm
+load --module 4 $HOME/os_lab_stages/stage_21/module_4.xsm
+load --module 5 $HOME/os_lab_stages/stage_21/module_5.xsm
+load --module 7 $HOME/os_lab_stages/stage_21/module_7.xsm
