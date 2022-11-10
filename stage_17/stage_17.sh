e29d660e92dfefeee4c6d2689cf8757bdf5e07f5
Mohammed Shaheem C <77876823+Shaheem589@users.noreply.github.com>
2022-11-10 12:01:36 +0530

    Add files via upload

---
+++ stage_17/stage_17.sh
@@ -0,0 +1,15 @@
+load --idle $HOME/os_lab_stages/stage_17/idle_process.xsm
+load --init $HOME/os_lab_stages/stage_17/gcd.xsm
+load --os $HOME/os_lab_stages/stage_17/os_startup.xsm
+load --int=timer $HOME/os_lab_stages/stage_17/timer.xsm
+load --int=console $HOME/os_lab_stages/stage_17/console.xsm
+load --int=6 $HOME/os_lab_stages/stage_17/int_6.xsm
+load --int=7 $HOME/os_lab_stages/stage_17/int_7.xsm
+load --int=9 $HOME/os_lab_stages/stage_17/int_9.xsm
+load --int=10 $HOME/os_lab_stages/stage_17/int_10.xsm
+load --module 0 $HOME/os_lab_stages/stage_17/module_0.xsm
+load --module 1 $HOME/os_lab_stages/stage_17/module_1.xsm
+load --module 2 $HOME/os_lab_stages/stage_17/module_2.xsm
+load --module 4 $HOME/os_lab_stages/stage_17/module_4.xsm
+load --module 5 $HOME/os_lab_stages/stage_17/module_5.xsm
+load --module 7 $HOME/os_lab_stages/stage_17/module_7.xsm
