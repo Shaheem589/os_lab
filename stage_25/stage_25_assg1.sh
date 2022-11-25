94f9f1ae15ef95fb5dee1e2c7ea62eab3ec3ec2d
Mohammed Shaheem C <77876823+Shaheem589@users.noreply.github.com>
2022-11-25 14:22:53 +0530

    Add files via upload

---
+++ stage_25/stage_25_assg1.sh
@@ -0,0 +1,27 @@
+load --idle $HOME/os_lab_stages/stage_25/idle_process.xsm
+load --init $HOME/os_lab_stages/stage_25/shell.xsm
+load --exec $HOME/os_lab_stages/stage_25/assg_1.xsm
+load --os $HOME/os_lab_stages/stage_25/os_startup.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/os_lab_stages/stage_25/exhandler.xsm
+load --int=timer $HOME/os_lab_stages/stage_25/timer.xsm
+load --int=console $HOME/os_lab_stages/stage_25/console.xsm
+load --int=disk $HOME/os_lab_stages/stage_25/disk.xsm
+load --int=4 $HOME/os_lab_stages/stage_25/int_4.xsm
+load --int=5 $HOME/os_lab_stages/stage_25/int_5.xsm
+load --int=6 $HOME/os_lab_stages/stage_25/int_6.xsm
+load --int=7 $HOME/os_lab_stages/stage_25/int_7.xsm
+load --int=8 $HOME/os_lab_stages/stage_25/int_8.xsm
+load --int=9 $HOME/os_lab_stages/stage_25/int_9.xsm
+load --int=10 $HOME/os_lab_stages/stage_25/int_10.xsm
+load --int=11 $HOME/os_lab_stages/stage_25/int_11.xsm
+load --int=13 $HOME/os_lab_stages/stage_25/int_13.xsm
+load --int=14 $HOME/os_lab_stages/stage_25/int_14.xsm
+load --int=15 $HOME/os_lab_stages/stage_25/int_15.xsm
+load --module 0 $HOME/os_lab_stages/stage_25/module_0.xsm
+load --module 1 $HOME/os_lab_stages/stage_25/module_1.xsm
+load --module 2 $HOME/os_lab_stages/stage_25/module_2.xsm
+load --module 3 $HOME/os_lab_stages/stage_25/module_3.xsm
+load --module 4 $HOME/os_lab_stages/stage_25/module_4.xsm
+load --module 5 $HOME/os_lab_stages/stage_25/module_5.xsm
+load --module 7 $HOME/os_lab_stages/stage_25/module_7.xsm
